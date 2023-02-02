08817b3d634de5c3927cdd8d4b6afdca4b355b23
Larissa Said Rocha <64704731+larisr@users.noreply.github.com>
2023-02-02 19:46:50 -0300

    Revert "Pesquisa screen"

--- map_for_u/android/app/src/main/kotlin/com/example/map_for_u/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.map_for_u
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- map_for_u/lib/data/botoes_menu_texto.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter/material.dart';
-
-import '../models/botao_menu.dart';
-
-const BOTOES_MENU = const [
-  BotaoMenu(id: '0', title: 'PESQUISAR', color: Colors.pinkAccent),
-  BotaoMenu(id: '1', title: 'CADASTRO', color: Colors.yellow),
-  BotaoMenu(id: '2', title: 'CONFIGURAÇÕES', color: Colors.purple),
-];
